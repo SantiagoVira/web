199eb48077ab3ef486192b6f6459d7bc891de646
SantiagoVira <69999123+SantiagoVira@users.noreply.github.com>
2020-08-25 19:46:41 -0400

    Update logins.js

--- logins.js
+++ logins.js
@@ -1,5 +1,5 @@
 let file = fopen("Accounts.txt", 3);// opens the file for writing
-let str = fread(file,flength(file) ;
+let str = fread(file,flength(file));
 function GetAcc(){
   let usr=document.getElementById("username").value
   let pass=document.getElementById("password").value
