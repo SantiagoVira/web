a5dac2647b4e1fb7d6f19b909f17c724bd0294c7
SantiagoVira <69999123+SantiagoVira@users.noreply.github.com>
2020-08-25 19:40:49 -0400

    Update logins.js

--- logins.js
+++ logins.js
@@ -1,1 +1,7 @@
-
+let file = fopen("Accounts.txt", 3);// opens the file for writing
+let str = fread(file,flength(file) ;
+function GetAcc(){
+  let usr=document.getElementById("username").value
+  let pass=document.getElementById("password").value
+}
+fwrite(file, str);// str is the content that is to be written into the file.
