96814cf50f7d17138d12ae6512bb72a3c3327f59
SantiagoVira <69999123+SantiagoVira@users.noreply.github.com>
2020-08-25 20:06:34 -0400

    Update logins.js

--- logins.js
+++ logins.js
@@ -1,9 +1,10 @@
-let file = fopen("Accounts.txt", 3);// opens the file for writing
-let str = fread(file,flength(file));
-function GetAcc(){
-  let usr=document.getElementById("username").value
-  let pass=document.getElementById("password").value
-  
-  fwrite(file, 'Hello World!');// str is the content that is to be written into the file.
-}
+let storedItem=localStorage.getItem('storedItem');
 
+function GetAcc(){
+  let user=document.getElementById('username').value;
+  let pass=document.getElementById('password').value;
+  
+  console.log(user+' '+pass)
+  //item='blah blah blah'
+  //localStorage.setItem('storedItem', item)
+  }
