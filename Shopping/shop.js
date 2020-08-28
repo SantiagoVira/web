d26b9bc7acf9b856951f0db172fcbf763936be14
SantiagoVira <69999123+SantiagoVira@users.noreply.github.com>
2020-08-27 22:48:26 -0400

    Update shop.js

--- Shopping/shop.js
+++ Shopping/shop.js
@@ -1,29 +1,47 @@
+var vals={}
+var ItemRed='#E0607E'
+var ItemGreen='#86df0b'
+
 function reset(){
     //gets rid of the lists
     localStorage.setItem('storedItem', JSON.stringify([]));
+    localStorage.setItem('Values', JSON.stringify([]));
+
     //gets rid of the stuff on the screen
     ClearLabels()
+}
+
+function Color(name, label){
+    if (vals[name] == true){
+        label.style.backgroundColor=ItemGreen
+      } else {
+        label.style.backgroundColor=ItemRed
+      }
 }
 
 function ClearLabels(){
     document.getElementById('buttons').innerHTML=''
 }
 
+function change(name, label){
+    vals[name]= !vals[name]
+    
+    localStorage.setItem('Values', JSON.stringify(vals));
+    Color(name, label)
+
+
+    
+}
 
 function AddItem(item){
     let Label=document.createElement('p');
     Label.appendChild(document.createTextNode(item))
     Label.className='buttons'
-
-    let Box=document.createElement('input');
-    Box.type='checkbox'
-    Box.className='checkboxes'
-    Label.appendChild(Box)
+    Label.id=item
+    Label.onclick=function() {change(item, Label)}
 
     let final=document.getElementById('buttons')
-    final.appendChild(Label)
-    //final.appendChild(Box)
-    //final.appendChild(document.createElement("br"))          
+    final.appendChild(Label)        
 
 }
 
@@ -31,8 +49,13 @@ function AddAll(){
     let Item=JSON.parse(localStorage.getItem('storedItem'));
     for (index = 0; index < Item.length; index++) { 
         AddItem(Item[index]); 
-        console.log(Item[index])
     } 
+    var vals=JSON.parse(localStorage.getItem('Values'))
+    let names=Object.keys(vals)
+
+    for (ind = 0; ind < names.length; ind++) {
+        Color(names[ind], document.getElementById(names[ind]));
+    }
 }
 
 function Add(){
@@ -40,10 +63,14 @@ function Add(){
     let storedItem=JSON.parse(localStorage.getItem('storedItem'));
     let NewItem=document.getElementById('item').value;
 
+    
     let all =storedItem
     all.push(NewItem)
 
+    vals[NewItem]=false;
+
     localStorage.setItem('storedItem', JSON.stringify(all));
+    localStorage.setItem('Values', JSON.stringify(vals));
 
     ClearLabels()
     AddAll()
@@ -51,7 +78,10 @@ function Add(){
     document.getElementById('item').value = ''
     }
 
-function remove(){
-    //for the buttons to mark it as gotten
-    null;
-}
+    function empty() {
+        if(document.getElementById("item").value==="") { 
+               document.getElementById('addbutton').disabled = true; 
+           } else { 
+               document.getElementById('addbutton').disabled = false;
+           }
+       }
